4cf62092984e341e309b32ce1482ff1060e77884
Vinay <vinayknp.iiita@gmail.com>
2016-03-06 21:59:41 +0530

    updating code- for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,14 @@ Rails.application.routes.draw do
   mount SabisuRails::Engine => "/sabisu_rails"
   devise_for :users
   namespace :api, path: '/' do
-    scope module: :v1,
-          constraints: ApiConstraints.new(version: 1, default: true) do
+  #  scope module: :v1,
+   #       constraints: ApiConstraints.new(version: 1, default: true) do
 
 
       resources :users, :only => [:show, :create, :update, :destroy]
       resources :sessions, :only => [:create, :destroy]
 
-    end
+    #end
     # We are going to list our resources here
   end
 end
